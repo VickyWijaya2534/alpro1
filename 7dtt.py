77c6c6a50dbd098f02af37e3fe98520e6aa44977
VickyWijaya2534 <149124588+VickyWijaya2534@users.noreply.github.com>
2023-10-30 20:57:27 +0700

    Update 7dtt.py

--- 7dtt.py
+++ 7dtt.py
@@ -32,57 +32,6 @@ print("""\n
       |3 |        Large          |
       ----------------------------""")
 
-bill = 0
-#order pizza
-while True:
-    order = int(input("\nMasukkan nomor pizza yang anda pesan : ")) #input pemesanan pizza
-    if order == 1 or 2 or 3 or 4 or 5 or 6 or 7 or 8:
-        bill += 43636
-    else:
-        print("format yang anda masukkan salah!")
-        break
-    crust = input("Masukkan nomor pilihan crust : ") #input pilihan crust
-    if crust == '1':
-        bill += 0
-    elif crust == '2':
-        bill += 11819
-    elif crust == '3':
-        bill += 9091
-    elif crust == '4':
-        bill += 13637
-    else:
-        print("Format yang anda masukkan tidak sesuai!")
-        break
-    ukuran = input("Masukkan nomor pilihan ukuran pizza : ") #input pilihan ukuran
-    if ukuran == '1':
-        bill += 0
-        ekstra_cheese = input("apakah anda ingin Ekstra cheese? (y/n) : ")
-        if ekstra_cheese == 'y':
-            bill += 13636
-        else:
-            bill += 0
-    elif ukuran == '2':
-        bill += 57273
-        ekstra_cheese = input("apakah anda ingin Ekstra cheese? (y/n) : ")
-        if ekstra_cheese == 'y':
-            bill += 16364
-        else:
-            bill += 0
-    elif ukuran == '3':
-        bill += 89091
-        ekstra_cheese = input("apakah anda ingin Ekstra cheese? (y/n) : ")
-        if ekstra_cheese == 'y':
-            bill += 19091
-        else:
-            bill += 0
-    else:
-        print("Format yang anda masukkan tidak sesuai!")
-        break
-    lanjut = input("pesan yang lain? (y/n) : ")
-    if lanjut =='n':
-        break
-print(f"""\nTerima kasih telah menggunakan layanan Pizza Hut delivery
-total tagihan anda adalah : Rp{bill}""")
 
 
 
